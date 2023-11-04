f87abfa0b363097f42175e06331363834a6b836a
clinton1719 <clintoncleetus17@gmail.com>
2023-11-04 20:04:00 +0530

    feat(INDXLIII-00002): Adding session store

--- ui/src/app/view_pr/page.tsx
+++ ui/src/app/view_pr/page.tsx
@@ -1,10 +1,14 @@
 import { fetchPRs } from '../../serviceLayer/apiCalls';
+import { getSessionValue, setSessionValue } from '../../utils/session-store';
 
 const ViewPRs = async () => {
   const response: any = await fetchPRs();
+  await setSessionValue('key', 'value');
+  const ans: any = await getSessionValue('key');
   return (
     <>
       <div>View PR</div>
+      <p>{ans}</p>
       {response.map((object: any, i: any) => (
         <p key={i}>{object.title}</p>
       ))}
---
+++ ui/src/utils/session-store.ts
@@ -0,0 +1,68 @@
+'use server';
+import { cookies } from 'next/headers';
+import { kv } from '@vercel/kv';
+
+type SessionId = string;
+
+/* Retreiving session ID starts*/
+export async function getSessionId(): Promise<string | undefined> {
+  await kv.set('user_1_session', 'session_token_value');
+  const cookieStore = cookies();
+  return cookieStore.get('session-id')?.value;
+}
+
+async function setSessionId(sessionId: SessionId): Promise<void> {
+  const cookieStore = cookies();
+  cookieStore.set('session-id', sessionId);
+}
+
+export async function getSessionIdAndCreateIfMissing(): Promise<
+  string | undefined
+> {
+  await kv.set('user_1_session', 'session_token_value');
+  const sessionId = getSessionId();
+  if (!sessionId) {
+    const newSessionId =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    setSessionId(newSessionId);
+
+    return newSessionId;
+  }
+
+  return sessionId;
+}
+/* Retreiving session ID ends*/
+
+/* Setting and Getting values from store starts */
+export async function getSessionValue(
+  key: string,
+  namespace: string = ''
+): Promise<string | undefined | null> {
+  const sessionId = getSessionId();
+  if (!sessionId) {
+    return null;
+  }
+  return kv.hget(`session-${namespace}-${sessionId}`, key);
+}
+
+export async function getAllSessionValues(
+  namespace: string = ''
+): Promise<Record<string, unknown> | null> {
+  const sessionId = getSessionId();
+  if (!sessionId) {
+    return null;
+  }
+  return kv.hgetall(`session-${namespace}-${sessionId}`);
+}
+
+export async function setSessionValue(
+  key: string,
+  value: string,
+  namespace: string = ''
+): Promise<number> {
+  const sessionId = getSessionIdAndCreateIfMissing();
+  return kv.hset(`session-${namespace}-${sessionId}`, { [key]: value });
+}
+
+/* Setting and Getting values from store ends */
