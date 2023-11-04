5c657a411a5eaf2d2054bd7afd778bde07f449ea
clinton1719 <clintoncleetus17@gmail.com>
2023-11-04 16:29:15 +0530

    feat(INDXLIII-00002): Fixing build step, adding env

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_VIEW_PR_URL: process.env.NEXT_PUBLIC_VIEW_PR_URL,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- ui/src/app/view_pr/page.tsx
+++ ui/src/app/view_pr/page.tsx
@@ -1,3 +1,5 @@
+'use static';
+
 const fetchPRs = async () => {
   const response = await fetch(process.env.NEXT_PUBLIC_VIEW_PR_URL!, {
     method: 'GET',
