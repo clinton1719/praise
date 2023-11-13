f9097771019f64185c8dea5f469b1b38c1a6d6ca
clinton1719 <clintoncleetus17@gmail.com>
2023-11-13 16:39:52 +0530

    Testing

--- integration-test/cypress/e2e/add_repo.cy.js
+++ integration-test/cypress/e2e/add_repo.cy.js
@@ -3,12 +3,9 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 
 describe('Add Repo should work', () => {
-  beforeEach(() => {
+  it('Should have 0 repos and all HTML rendered', () => {
     cy.visit('/add_repo');
     cy.wait(5000);
-  });
-
-  it('Should have 0 repos and all HTML rendered', () => {
     cy.url().should('include', '/add_repo');
     cy.contains('PRaise').should('be.visible');
     cy.contains('Add').should('be.visible');
@@ -17,7 +14,10 @@ describe('Add Repo should work', () => {
   });
 
   it('Should take input and submit', () => {
+    cy.visit('/add_repo');
+    cy.wait(5000);
     cy.get('.inputPr').type('aws/aws-cdk');
+    cy.wait(3000);
     cy.get('.inputPr').should('have.value', 'aws/aws-cdk');
 
     cy.get('#addPrButton').click({ force: true });
