08018d31c1d9dde5a9cc900769a19ac14eb75996
clinton1719 <clintoncleetus17@gmail.com>
2023-11-14 10:44:43 +0530

    Test

--- integration-test/cypress/e2e/add_repo.cy.js
+++ integration-test/cypress/e2e/add_repo.cy.js
@@ -13,17 +13,17 @@ describe('Add Repo should work', () => {
     cy.contains('List of repos added: 0').should('be.visible');
   });
 
-  // it('Should take input and submit', () => {
-  //   cy.visit('/add_repo');
-  //   cy.wait(5000);
-  //   cy.get('.inputPr').type('aws/aws-cdk');
-  //   cy.get('.inputPr').should('have.value', 'aws/aws-cdk');
+  it('Should take input and submit', () => {
+    cy.visit('/add_repo');
+    cy.wait(5000);
+    cy.get('.inputPr').type('aws/aws-cdk');
+    cy.get('.inputPr').should('have.value', 'aws/aws-cdk');
 
-  //   cy.get('#addPrButton').click({ force: true });
-  //   cy.wait(10000);
-  //   cy.reload();
-  //   cy.wait(10000);
-  //   cy.contains('aws/aws-cdk').should('be.visible');
-  //   cy.contains('List of repos added: 1').should('be.visible');
-  // });
+    cy.get('#addPrButton').click({ force: true });
+    cy.wait(10000);
+    cy.reload();
+    cy.wait(10000);
+    cy.contains('aws/aws-cdk').should('be.visible');
+    cy.contains('List of repos added: 1').should('be.visible');
+  });
 });
--- integration-test/cypress/e2e/view_pr.cy.js
+++ integration-test/cypress/e2e/view_pr.cy.js
@@ -3,21 +3,21 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 
 describe('View Repo should work', () => {
-  // it('Should load and render HTML properly', () => {
-  //   cy.visit('/add_repo');
-  //   cy.get('.inputPr').type('aws/aws-cdk');
-  //   cy.get('.inputPr').should('have.value', 'aws/aws-cdk');
-  //   cy.get('#addPrButton').click({ force: true });
-  //   cy.wait(10000);
-  //   cy.reload();
-  //   cy.wait(10000);
-  //   cy.visit('/view_pr');
-  //   cy.wait(10000);
-  //   cy.contains('Repo Name').should('be.visible');
-  //   cy.contains('Title').should('be.visible');
-  //   cy.contains('URL').should('be.visible');
-  //   cy.contains('Requester').should('be.visible');
-  //   cy.contains("View PR's").should('be.visible');
-  //   cy.contains('aws/aws-cdk').should('be.visible');
-  // });
+  it('Should load and render HTML properly', () => {
+    cy.visit('/add_repo');
+    cy.get('.inputPr').type('aws/aws-cdk');
+    cy.get('.inputPr').should('have.value', 'aws/aws-cdk');
+    cy.get('#addPrButton').click({ force: true });
+    cy.wait(10000);
+    cy.reload();
+    cy.wait(10000);
+    cy.visit('/view_pr');
+    cy.wait(10000);
+    cy.contains('Repo Name').should('be.visible');
+    cy.contains('Title').should('be.visible');
+    cy.contains('URL').should('be.visible');
+    cy.contains('Requester').should('be.visible');
+    cy.contains("View PR's").should('be.visible');
+    cy.contains('aws/aws-cdk').should('be.visible');
+  });
 });
